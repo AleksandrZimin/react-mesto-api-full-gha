ef6cc18dda19a2ae76f033b68acd90c72570fa15
Aleksandr Zimin <uzao-rec@ya.ru>
2023-10-07 01:13:06 +0300

    Добавляю папку build

---
+++ frontend/build/precache-manifest.fdbd323587486d67d507093e5bbb890d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5a2ff8d549f5b775ff",
+    "url": "/static/css/main.724b6092.chunk.css"
+  },
+  {
+    "revision": "ec5a2ff8d549f5b775ff",
+    "url": "/static/js/main.1d51f01e.chunk.js"
+  },
+  {
+    "revision": "0f75e35ebfda020440a1",
+    "url": "/static/js/runtime~main.59317b4d.js"
+  },
+  {
+    "revision": "b072f371e4aeca2235a5",
+    "url": "/static/js/2.410a4a3b.chunk.js"
+  },
+  {
+    "revision": "599d3d9fdab81ba68257",
+    "url": "/static/js/3.ce021d70.chunk.js"
+  },
+  {
+    "revision": "51772f7cae792434c441016d1f475dda",
+    "url": "/static/media/logo.51772f7c.svg"
+  },
+  {
+    "revision": "48bb334e9ca31b6263593527bb6314ac",
+    "url": "/static/media/delete.48bb334e.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/Vector(1).bdfb8ebb.svg"
+  },
+  {
+    "revision": "fa6594a6601f34cbfad1ca8b1f00c022",
+    "url": "/static/media/Inter-Black.fa6594a6.woff2"
+  },
+  {
+    "revision": "f7656ebefaa01fb7d1f53e221d01ecc1",
+    "url": "/static/media/Inter-Medium.f7656ebe.woff2"
+  },
+  {
+    "revision": "f8d6b387df318f9ff8ba1d6afdf2b506",
+    "url": "/static/media/Inter-Medium.f8d6b387.woff"
+  },
+  {
+    "revision": "eac1ef330b65c2c5bcd2f901534a2e3a",
+    "url": "/static/media/Inter-Black.eac1ef33.woff"
+  },
+  {
+    "revision": "b6204e253064e05f23f58f46e9d32d8d",
+    "url": "/static/media/Inter-Regular.b6204e25.woff2"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/avatar-hover.34c46850.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "/static/media/Edit-Button.c8252db2.svg"
+  },
+  {
+    "revision": "1c222b13f6d36ec40c4149ffc566d94e",
+    "url": "/static/media/Inter-Regular.1c222b13.woff"
+  },
+  {
+    "revision": "933106eaf93ffbb3807048cfeeecbcef",
+    "url": "/static/media/heart-black.933106ea.svg"
+  },
+  {
+    "revision": "4c6a21695c10c9c77e7676ffa4a389c1",
+    "url": "/static/media/Close-Icon.4c6a2169.svg"
+  },
+  {
+    "revision": "cb5f16439886c2d2781c07c597fe5b1a",
+    "url": "/static/media/Group.cb5f1643.svg"
+  },
+  {
+    "revision": "2380dc1aaa816d36eb7f8cde95d46e47",
+    "url": "/index.html"
+  }
+];
